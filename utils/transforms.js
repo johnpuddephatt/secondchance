5b01c5be17a518449ee2113009052c5eadba0f0d
John Puddephatt <john@jdp.org.uk>
2021-07-01 17:20:41 +0100

    volunteering form etc

--- utils/transforms.js
+++ utils/transforms.js
@@ -3,42 +3,44 @@ const critical = require('critical')
 const buildDir = 'dist'
 
 const shouldTransformHTML = (outputPath) =>
-    outputPath &&
-    outputPath.endsWith('.html') &&
-    process.env.ELEVENTY_ENV === 'production'
+  outputPath &&
+  outputPath.endsWith('.html') &&
+  process.env.ELEVENTY_ENV === 'production'
 
 const isHomePage = (outputPath) => outputPath === `${buildDir}/index.html`
 
 process.setMaxListeners(Infinity)
 module.exports = {
-    htmlmin: function (content, outputPath) {
-        if (shouldTransformHTML(outputPath)) {
-            return htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
-                collapseWhitespace: true
-            })
-        }
-        return content
-    },
-
-    critical: async function (content, outputPath) {
-        if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
-         try {
-            const {css, html, uncritical} = await critical.generate({
-              base: `${buildDir}/`,
-              html: content,
-              width: 1401,
-              height: 900,
-              inline: true
-            });
-            return html;
-          } catch (err) {
-            console.error(err);
-          }
-        }
-        else {
-          return content;
-        }
+  htmlmin: function (content, outputPath) {
+    if (shouldTransformHTML(outputPath)) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
     }
+    return content
+  },
+
+  critical: async function (content, outputPath) {
+    if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
+      try {
+        const { css, html, uncritical } = await critical.generate({
+          base: `${buildDir}/`,
+          html: content,
+          width: 1401,
+          height: 900,
+          inline: true,
+          penthouse: {
+            timeout: 300000
+          }
+        })
+        return html
+      } catch (err) {
+        console.error(err)
+      }
+    } else {
+      return content
+    }
+  }
 }
